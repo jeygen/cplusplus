676b94bca2b0169fa67fe1364c2a1960390a09cc
babemac <joshua.eygenraam@ryerson.ca>
2023-01-05 17:06:16 -0500

    growing the repo baby

---
+++ compiling.md
@@ -0,0 +1,2 @@
+Use g++ as it is equiv to "gcc -xc++ -lstdc++ -shared-libgcc"
+g++ takes care of linking issues
--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // standard library, includes cout, etc.
+using namespace std; // can use direct standared libary names
 
 int main() {
-  cout << "Hello World!";
+  cout << "Hello World!\n"; // see-out with insertion << operator 
+  //std::cout << "Hello World"; // if namespace is not declared
   return 0;
 } 
