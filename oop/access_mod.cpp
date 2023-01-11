414d07dad62c2ff3d10e91eb5b7116426d302bc9
babemac <joshua.eygenraam@ryerson.ca>
2023-01-11 13:59:24 -0500

    added access mods

---
+++ oop/access_mod.cpp
@@ -0,0 +1,14 @@
+/*
+ * Like java, but use a colon
+ */
+
+class Example {
+ public:
+  int a; // public member
+  void foo(); //applies to methods too
+ private:
+  int b; // private member
+ protected:
+  int c; // protected member
+};
+
