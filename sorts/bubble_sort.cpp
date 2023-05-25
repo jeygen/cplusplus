19d163ed48c8488145231e65a77ddacab11cbfaa
debra <debra@debby.myguest.virtualbox.org>
2023-05-24 22:11:50 -0400

    added vector bubble sort

--- sorts/bubble_sort.cpp
+++ sorts/bubble_sort.cpp
@@ -28,3 +28,19 @@ int main() {
    printArray(arr, n);
    return 0;
 }
+
+#include <vector>
+#include <algorithm>
+
+void bubbleSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for(int i = 0; i < n-1; i++) {     
+        for (int j = 0; j < n-i-1; j++) { 
+            if (arr[j] > arr[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                std::swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
