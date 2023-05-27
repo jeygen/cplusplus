7dd2a60bf3fd8e414600ee346b8f04eb4d58a58d
debra <debra@debby.myguest.virtualbox.org>
2023-05-27 12:45:31 -0400

    added iterators and searches

---
+++ iterator.cpp
@@ -0,0 +1,64 @@
+/*
+    Generality: 
+		**Iterators allow for writing code that can work with any container type**
+
+    Abstraction: 
+		They hide the specifics of the underlying container, allowing the same code to be used with different container types.
+
+    Flexibility: 
+		Different types of iterators (input, output, forward, bidirectional, random access) provide varying levels of functionality based on the needs of the algorithm.
+
+    Efficiency: 
+		Some operations can be performed more efficiently with iterators, such as inserting or deleting elements in the middle of a container.
+
+**value you get when you dereference an iterator is a reference to the element in the container that the iterator is pointing to, and the type of this reference matches the type of the elements in the containe
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+
+int main() {
+    // Let's create a vector
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+
+    // Let's print the elements of the vector using iterators
+    for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it) {
+        std::cout << *it << ' ';
+    }
+    std::cout << '\n';
+
+    // Now, let's modify the vector elements using iterators
+    for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it) {
+        *it += 5;  // Add 5 to each element
+    }
+
+    // Print the modified vector
+    for (const auto& elem : vec) {
+        std::cout << elem << ' ';
+    }
+    std::cout << '\n';
+
+    // Let's create a set
+    std::set<int> s = {5, 1, 3, 2, 4};
+
+    // Print the set using iterators
+    for (std::set<int>::iterator it = s.begin(); it != s.end(); ++it) {
+        std::cout << *it << ' ';
+    }
+    std::cout << '\n';
+
+    // Let's create a list
+    std::list<int> lst = {1, 2, 3, 4, 5};
+
+    // Print the list using iterators
+    for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it) {
+        std::cout << *it << ' ';
+    }
+    std::cout << '\n';
+
+    return 0;
+}
+
---
+++ search.cpp
@@ -0,0 +1,72 @@
+// Homemade searches
+
+#include <bits/stdc++.h> // all std libs, gcc only tho
+using namespace std;
+
+// O(n)
+int linearSearch(int arr[], int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// O(logn)
+int binarySearch(int arr[], int l, int r, int x) {
+    if (r >= l) {
+		// mid calc prevents overflow
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == x) {
+            return mid;
+        }
+        if (arr[mid] > x) {
+            return binarySearch(arr, l, mid - 1, x);
+        }
+        return binarySearch(arr, mid + 1, r, x);
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 10, 40};
+    int x = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = linearSearch(arr, n, x);
+    (result == -1) ? cout << "Element is not present in array"
+                   : cout << "Element is present at index " << result;
+    int result = binarySearch(arr, n, x);
+    (result == -1) ? cout << "Element is not present in array"
+                   : cout << "Element is present at index " << result;
+
+
+// can use algorithm library for these as well
+	// find() for linear
+
+    vector<int> v = {2, 3, 4, 10, 40};
+    int x2 = 10;
+
+    auto it = find(v.begin(), v.end(), x2);
+    if (it != v.end())
+        cout << "Element found at index " << distance(v.begin(), it) << "\n";
+    else
+        cout << "Element not found\n";
+
+	// binary_search()
+		// returns boolean if found in seq
+		// lower_b() returns iterator of location or i of location where should be
+		// these functions only work with sorted content
+
+	    if (binary_search(v.begin(), v.end(), x)) {
+        auto it = lower_bound(v.begin(), v.end(), x);
+        cout << "Element found at index " << distance(v.begin(), it) << "\n";
+    } else {
+        cout << "Element not found\n";
+    }
+
+
+
+    return 0;
+}
+
