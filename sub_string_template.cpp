d692554421670ae911a73af48d5520b455d320f7
debra <debra@debby.myguest.virtualbox.org>
2023-05-29 22:31:31 -0400

    add ss templates and examples

---
+++ sub_string_template.cpp
@@ -0,0 +1,73 @@
+int findSubstring(string s){
+        vector<int> map(128,0);
+        int counter; // check whether the substring is valid
+        int begin=0, end=0; //two pointers, one point to tail and one head
+        int d; //the length of substring
+
+        for() { /* initialize the hash map here */ }
+
+        while(end<s.size()){
+
+            if(map[s[end++]]-- ?){  /* modify counter here */ }
+
+            while(/* counter condition */){ 
+                 
+                 /* update d here if finding minimum*/
+
+                //increase begin to make it invalid/valid again
+                
+                if(map[s[begin++]]++ ?){ /*modify counter here*/ }
+            }  
+
+            /* update d here if finding maximum*/
+        }
+        return d;
+  }
+
+// Longest pattern, no char repeats
+int lengthOfLongestSubstring(string s) {
+    vector<int> map(128, 0);
+    int counter = 0;
+    int begin = 0, end = 0, d = 0;
+
+    while (end < s.size()) {
+        if (map[s[end]]++ > 0) counter++;
+        end++;
+
+        while (counter > 0) {
+            if (map[s[begin]]-- > 1) counter--;
+            begin++;
+        }
+        d = max(d, end - begin); // Update d here if finding maximum
+    }
+    return d;
+}
+
+// longest with at most 2 distinct chars
+int lengthOfLongestSubstringTwoDistinct(string s) {
+        vector<int> map(128, 0);
+        int counter=0, begin=0, end=0, d=0; 
+        while(end<s.size()){
+            if(map[s[end++]]++==0) counter++;
+            while(counter>2) if(map[s[begin++]]--==1) counter--;
+            d=max(d, end-begin);
+        }
+        return d;
+}
+
+// return size of window that contains 2 given strings
+string minWindow(string s, string t) {
+        vector<int> map(128,0);
+        for(auto c: t) map[c]++;
+        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
+        while(end<s.size()){
+            if(map[s[end++]]-->0) counter--; //in t
+            while(counter==0){ //valid
+                if(end-begin<d)  d=end-(head=begin);
+                if(map[s[begin++]]++==0) counter++;  //make it invalid
+            }  
+        }
+        return d==INT_MAX? "":s.substr(head, d);
+}
+
+
