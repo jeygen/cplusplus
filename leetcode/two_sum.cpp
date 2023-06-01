9d94d2a2fdb470c37a6c0ab92c7a9ca0a0fc481a
debra <debra@debby.myguest.virtualbox.org>
2023-05-31 22:51:19 -0400

    updated leet and ss

--- leetcode/two_sum.cpp
+++ leetcode/two_sum.cpp
@@ -38,3 +38,24 @@ vector<int> twoSum(vector<int>& nums, int target) {
 	}
 	return ans;
 }
+
+// faster
+
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (map.find(complement) != map.end()) {
+                return {map[complement], i};
+            }
+            map[nums[i]] = i;
+        }
+        return {};  // Return an empty vector if no solution is found
+    }
+};
+
--- sub_string_template.cpp
+++ sub_string_template.cpp
@@ -1,73 +1,76 @@
+// any spare d's are now sub string length
+
 int findSubstring(string s){
         vector<int> map(128,0);
         int counter; // check whether the substring is valid
-        int begin=0, end=0; //two pointers, one point to tail and one head
-        int d; //the length of substring
+        int begin_index=0, end_index=0; //two pointers, one point to tail and one head
+        int sub_len; //the length of substring
 
         for() { /* initialize the hash map here */ }
 
-        while(end<s.size()){
+        while(end_index<s.size()){
 
-            if(map[s[end++]]-- ?){  /* modify counter here */ }
+            if(map[s[end_index++]]-- ?){  /* modify counter here */ }
 
             while(/* counter condition */){ 
                  
-                 /* update d here if finding minimum*/
+                 /* update sub_len here if finding minimum*/
 
-                //increase begin to make it invalid/valid again
+                //increase begin_index to make it invalid/valid again
                 
-                if(map[s[begin++]]++ ?){ /*modify counter here*/ }
+                if(map[s[begin_index++]]++ ?){ /*modify counter here*/ }
             }  
 
-            /* update d here if finding maximum*/
+            /* update sub_len here if finding maximum*/
         }
-        return d;
+        return sub_len;
   }
 
 // Longest pattern, no char repeats
 int lengthOfLongestSubstring(string s) {
-    vector<int> map(128, 0);
+    vector<int> map(128, 0); // make dynamic array of 128 as asci is 0-127
     int counter = 0;
-    int begin = 0, end = 0, d = 0;
+    int begin_index = 0, end_index = 0, sub_len = 0;
 
-    while (end < s.size()) {
-        if (map[s[end]]++ > 0) counter++;
-        end++;
+    while (end_index < s.size()) {
+		// this syntax is increment everytime its run, counter iff success
+        if (map[s[end_index]]++ > 0) counter++;
+        end_index++;
 
         while (counter > 0) {
-            if (map[s[begin]]-- > 1) counter--;
-            begin++;
+            if (map[s[begin_index]]-- > 1) counter--;
+            begin_index++;
         }
-        d = max(d, end - begin); // Update d here if finding maximum
+        sub_len = max(sub_len, end_index - begin_index); // Update d here if finding maximum
     }
-    return d;
+    return sub_len;
 }
 
-// longest with at most 2 distinct chars
+// longest with at most 2 sub_lenistinct chars
 int lengthOfLongestSubstringTwoDistinct(string s) {
         vector<int> map(128, 0);
-        int counter=0, begin=0, end=0, d=0; 
-        while(end<s.size()){
-            if(map[s[end++]]++==0) counter++;
-            while(counter>2) if(map[s[begin++]]--==1) counter--;
-            d=max(d, end-begin);
+        int counter=0, begin_index=0, end_index=0, sub_len=0; 
+        while(end_index<s.size()){
+            if(map[s[end_index++]]++==0) counter++;
+            while(counter>2) if(map[s[begin_index++]]--==1) counter--;
+            sub_len=max(sub_len, end_index-begin_index);
         }
-        return d;
+        return sub_len;
 }
 
 // return size of window that contains 2 given strings
 string minWindow(string s, string t) {
         vector<int> map(128,0);
         for(auto c: t) map[c]++;
-        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
-        while(end<s.size()){
-            if(map[s[end++]]-->0) counter--; //in t
+        int counter=t.size(), begin_index=0, end_index=0, sub_len=INT_MAX, head=0;
+        while(end_index<s.size()){
+            if(map[s[end_index++]]-->0) counter--; //in t
             while(counter==0){ //valid
-                if(end-begin<d)  d=end-(head=begin);
-                if(map[s[begin++]]++==0) counter++;  //make it invalid
+                if(end_index-begin_index<sub_len)  sub_len=end_index-(head=begin_index);
+                if(map[s[begin_index++]]++==0) counter++;  //make it invalid
             }  
         }
-        return d==INT_MAX? "":s.substr(head, d);
+        return sub_len==INT_MAX? "":s.substr(head, d);
 }
 
 
