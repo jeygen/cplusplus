9ee5024b0d24d2280b0c65508dad317efcd21466
Joshua Eygenraam <83786292+jeygen@users.noreply.github.com>
2023-07-07 10:17:39 -0400

    Create heap.cpp

---
+++ data_structures/heap.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+void heapify(std::vector<int>& arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void buildHeap(std::vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+}
+
+int main() {
+    std::vector<int> arr = {4, 10, 3, 5, 1};
+
+    buildHeap(arr);
+
+    std::cout << "Max Heap: ";
+    for (int val : arr) {
+        std::cout << val << " ";
+    }
+
+    return 0;
+}
