73fa2a889b89ef731cb3be3024f80980c0d689db
josh <joshua.eygenraam@ryerson.ca>
2023-07-08 21:41:56 -0400

    added back-up cleaning

--- leetcode/invert_tree.cpp
+++ leetcode/invert_tree.cpp
@@ -15,3 +15,81 @@ void invertBinaryTree(vector<int>& tree) {
     }
 }
 
+TreeNode* invertTree(TreeNode* root) {
+    if (root == nullptr) {
+        return nullptr;
+    }
+
+    // swap left and right children
+    TreeNode* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+
+    // invert left and right subtrees
+    invertTree(root->left);
+    invertTree(root->right);
+
+    return root;
+}
+
+
+TreeNode* invertTree(TreeNode* root) {
+    if (root == nullptr) {
+        return nullptr;
+    }
+
+    // dont need visited array as no cycles in trees
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode* current = q.front();
+        q.pop();
+
+        // swap left and right children
+        TreeNode* temp = current->left;
+        current->left = current->right;
+        current->right = temp;
+
+        // if not a nullptr 
+        if (current->left) {
+            q.push(current->left);
+        }
+
+        if (current->right) {
+            q.push(current->right);
+        }
+    }
+
+    return root;
+}
+
+TreeNode* invertTree(TreeNode* root) {
+    if (root == nullptr) {
+        return nullptr;
+    }
+
+    stack<TreeNode*> s;
+    s.push(root);
+
+    while (!s.empty()) {
+        TreeNode* current = s.top();
+        s.pop();
+
+        // swap left and right children
+        TreeNode* temp = current->left;
+        current->left = current->right;
+        current->right = temp;
+
+        if (current->left) {
+            s.push(current->left);
+        }
+
+        if (current->right) {
+            s.push(current->right);
+        }
+    }
+
+    return root;
+}
+
