e99b87ca3a5f232269f5813ed1eb0566c3942b1f
josh <joshua.eygenraam@ryerson.ca>
2023-07-22 14:49:59 -0400

    added systems eng q

--- test_q/systems_eng_q.cpp
+++ test_q/systems_eng_q.cpp
@@ -14,4 +14,16 @@ Using C++, design a class hierarchy for a set of power-manageable devices with s
 Implement a C++ function that simulates memory allocation and deallocation in a system, managing memory fragmentation.
 
 Let's say you have to write an algorithm to optimize power usage in a system based on active hours. How would you design this algorithm in C++?
+
+It says verilog, but these can be c++
+
+    Assume that you have a binary file that represents a block of memory from an embedded system. Write a function in Verilog to read this file and search for a specific sequence of bytes.
+
+    In firmware development, reading and writing to hardware registers is a common task. Write a function in Verilog to read and write to a memory-mapped I/O device.
+
+    Implement a simple power management unit in Verilog that can turn on and off components based on some criteria.
+
+    How would you write a testbench in Verilog for validating a new System-on-Chip (SoC) design?
+
+    Design a simple machine learning accelerator in Verilog. How would you handle communication with the host system and data transfer?
 */
