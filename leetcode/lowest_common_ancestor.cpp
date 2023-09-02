9e2ad91db37a6693e83910edcc73af60ff0e62a5
root <joshua.eygenraam@torontomu.ca>
2023-09-03 03:12:58 +0800

    added lowest common ancestor

---
+++ leetcode/lowest_common_ancestor.cpp
@@ -0,0 +1,47 @@
+/*
+   235. Lowest Common Ancestor of a Binary Search Tree
+Medium
+10.2K
+281
+Companies
+
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+
+
+Example 1:
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+
+Example 2:
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+
+Example 3:
+
+Input: root = [2,1], p = 2, q = 1
+Output: 2
+*/
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q) return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (left && right) return root;  // both p and q found in left and right subtree
+        if (!left) return right;  // only right subtree contains either p or q or none
+        if (!right) return left;  // only left subtree contains either p or q or none
+
+        return nullptr;  // to satisfy all return paths
+    }
+};
+
