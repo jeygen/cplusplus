a3696b86f1d48b60528fc7477bf49431dd36c5d0
jeygen <83786292+jeygen@users.noreply.github.com>
2023-11-14 21:22:09 -0500

    updated singleton

--- design_patterns/singleton.cpp
+++ design_patterns/singleton.cpp
@@ -53,3 +53,48 @@ public:
 // Initialize the static member variable
 Singleton* Singleton::instance = nullptr;
 
+
+// chatgpt buddy
+
+class Singleton {
+private:
+    // Private Constructor
+    // This prevents anyone outside the class from creating a new instance.
+    Singleton() {}
+
+    // Private Copy constructor and assignment operator
+    // These are deleted to prevent copying of the singleton instance.
+    // Deleting these methods ensures that you can't create a copy of the singleton.
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+public:
+    // Static method to access the instance
+    // This method is static, meaning it can be called on the class itself,
+    // not on an instance of the class.
+    static Singleton& getInstance() {
+        // This static instance is created only once and then reused.
+        // The instance is created the first time this method is called.
+        // It is destroyed when the program exits.
+        static Singleton instance;
+
+        // Return a reference to the instance.
+        // Returning a reference ensures that no copy of the instance is made.
+        return instance;
+    }
+};
+
+int main() {
+    // Access the singleton instance
+    // Calling getInstance() will always return the same instance.
+    Singleton& s1 = Singleton::getInstance();
+
+    // Proof that s1 and s2 refer to the same instance
+    Singleton& s2 = Singleton::getInstance();
+    if (&s1 == &s2) {
+        // This will print, proving both references point to the same instance.
+        std::cout << "s1 and s2 are the same instance" << std::endl;
+    }
+
+    return 0;
+}
