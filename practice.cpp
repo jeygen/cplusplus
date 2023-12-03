7e5d1700f9e17aac85d93a35d84ce0f9da6dcd9a
jeygen <joshua.eygenraam@torontomu.ca>
2023-12-03 14:15:10 -0500

    edited prac.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,1 +1,52 @@
 //
+// swap at least 3 ways
+
+// fibo 
+
+// factorial
+
+// binary search
+
+// reverse string
+
+// fizzbuzz
+
+// bubble sorto
+
+// count chars in string
+
+// Palindrome Checker
+
+// Prime Number Checker
+
+// Count number of digits in integer and reverse
+
+// Array Sum
+
+// Find Minimum/Maximum
+
+// String Length without using `length()` or `size()`
+
+// Odd or Even
+
+// Convert Case
+
+// Duplicate Element Checker
+
+// Vowel Counter
+
+// Multiplication Table
+
+// Find the missing number
+
+// Find the duplicate number
+
+// Remove duplicates from a list
+
+// dot product
+
+// cross product
+
+// matrix mult
+
+// matrix add
